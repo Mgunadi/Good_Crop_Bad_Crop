a37779181e9c78fa5bf4e5a2221abc9d1a9f3969
Mgunadi <mgun_27@hotmail.com>
2021-09-19 12:29:35 +1000

    Modularised backend (data import and VI dictionary creation) and front end. Time series graph now shows NDVI.

--- dashboardv3/dashboard.py
+++ dashboardv3/dashboard.py
@@ -1,11 +1,4 @@
 import dash
-import pandas as pd
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-from matplotlib import image
-import glob
-import os
 #from dash import dcc
 #from dash import html
 import dash_core_components as dcc
@@ -13,7 +6,8 @@ import dash_html_components as html
 import boto3
 from skimage import io
 import plotly.express as px
-import random
+from create_VI_timeseries import create_VI
+
 
 # Instantiating the Dashboard Application
 app = dash.Dash(__name__)
@@ -26,111 +20,18 @@ KEY = 'satellite-data/phase-01/data/sentinel-2a-tile-7680x-10240y/timeseries/768
 # Outputs image file to current directory
 s3.Bucket(BUCKET_NAME).download_file(KEY, 'current_satellite_image.jpg')
 
-def get_tile_path(x, y, band, date):
-    path = f'C:\\Users\\Gladiator\\Documents\\Good_Crop_Bad_Crop\\model\\satelite_data\\phase-01\\data\\sentinel-2a-tile-{x}x-{y}y\\timeseries\\{x}-{y}-{band}-{date}.png'
-    return path
+# Create VI timeseries graph
+# TODO: Currently, just creates plot for NVDI, expand to all VIs
+labels ={"value": "Average", "variable": 'Statistic'}
+VI_dict = create_VI()
+NDVI = VI_dict['NDVI']
+VI_metric = str(list(VI_dict.keys())[0])
 
-def get_image(x, y, band, date):
-    path = get_tile_path(x,y,band,date)
-    im = image.imread(path)
-    return im
-
-paths = glob.glob(get_tile_path('7680','10240','B01','*'))
-dates = []
-for path in paths:
-    x = len(path)
-    dates.append(path[x-14:x-4])
-
-# sort the list of dates  
-dates.sort(key= lambda x: int(''.join(x.split('-'))))
-
-# Code the colour bands
-BLUE = 'B02'
-GREEN = 'B03'
-RED = 'B04'
-NIR = 'B08'
-
-# NVDI calculation
-def calulcate_NVDI(NIR, RED):
-    return (NIR - RED) / (NIR + RED)
-
-# ENVDI calcuation
-def calculate_ENVDI(NIR, GREEN, BLUE):
-    return ((NIR+GREEN) - (2*BLUE)) / ((NIR+GREEN) + (2*BLUE)) 
-
-# GNDVI calcuation
-def calulcate_GNDVI(NIR, GREEN):
-    return (NIR - GREEN) / (NIR + GREEN)
-
-def get_wavelength(x, y, band, date):
-    im = Image.open(get_tile_path(x,y,band,date))
-    arr = np.array(im.getdata()).reshape(512,512)
-    return arr
-
-def get_partition(image, xStart, yStart, mask):
-    result = [] 
-    for y in range(0, len(mask)):
-        for x in range(0, len(mask[0])):
-            # check if the mask pixel is selected then add the img pixel to list
-            if mask[y,x]==1:
-                result.append(image[yStart+y, xStart+x])
-    return np.array(result)
-
-
-def get_avg_vegetation_index(vi, X, Y, xStart, yStart, mask, date_range):
-    date_range = []
-    result = []
-    avg = []
-    upper = []
-    lower = []
-    for date in dates[0:12]:
-        red = get_wavelength(X,Y,RED,date)
-        blue = get_wavelength(X,Y,BLUE,date)
-        green = get_wavelength(X,Y,GREEN,date)
-        nir = get_wavelength(X,Y,NIR,date)
-        
-        if vi == 'NVDI':
-            nvdi = calulcate_NVDI(nir, red)
-            res = get_partition(nvdi, xStart, yStart, mask)
-            average = np.average(res)
-            sd = np.std(res)
-
-            avg.append(average)
-            upper.append(average+(2*sd))
-            lower.append(average-(2*sd))
-            result.append(res)
-    dict = {}
-    dict['result'] = result
-    dict['avg'] = avg
-    dict['upper'] = upper
-    dict['lower'] = lower
-    return dict
-
-# Data
-xlabels = [] 
-result = []
-xlabel2 = []
-X = '7680'
-Y = '10240'
-xStart = 0
-yStart = 0
-mask = np.ones((20,20))
-temp = get_avg_vegetation_index('NVDI', X, Y, xStart, yStart, mask, 2)
-for i in range(0, len(temp['result'])):
-    xlabel2.append(i)
-    for res in temp['result'][i]:
-        result.append(res)
-        xlabels.append(i)
-df = pd.DataFrame({'Stage': xlabel2, 'Average': temp['avg'], 'Upper': temp['upper'], 'Lower': temp['lower']})
-
-labels={"value": "Average", "variable": 'Statistic'}
-vi_fig = px.line(df, x='Stage', y=df.columns, labels=labels)
+vi_fig = px.line(NDVI, x='Stage', y=NDVI.columns, labels=labels, title=f"{VI_metric} Time Series of selected region")
 #fig.add_scatter()
 vi_fig.update_layout(autosize=True)
 
-
-
-
+# Create Satellite image to display
 title = 'The date this image was captured: xx/xx/xx'
 img = io.imread('current_satellite_image.jpg')
 map_fig = px.imshow(img)
@@ -138,10 +39,7 @@ map_fig.update_xaxes(showticklabels=False)
 map_fig.update_yaxes(showticklabels=False)
 map_fig.update_layout(autosize=True, margin=dict(l=0, r=0, b=0, t=0))
 
-
-
-
-#components
+# Dashboard Components
 vi_radio = dcc.RadioItems(
     options=[
         {'label': 'Phenological Stage', 'value': 'NDVI'},
