4316c0c37a708721e37a24cf42742206d7925f6e
Mgunadi <mgun_27@hotmail.com>
2021-09-24 18:21:24 +1000

    CSS style changes

--- dashboardv3/dashboard.py
+++ dashboardv3/dashboard.py
@@ -2,10 +2,6 @@ import dash
 import pandas as pd
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
-from matplotlib import image
-import glob
-import os
 #from dash import dcc
 #from dash import html
 import dash_core_components as dcc
@@ -17,8 +13,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
-# Instantiating the Dashboard Application
-app = dash.Dash(__name__)
 
 # S3 import implementation for Satellite image
 s3 = boto3.resource('s3')
@@ -26,6 +20,12 @@ BUCKET_NAME = 'goodcropbadcrop'
 KEY = 'satellite-data/phase-01/data/sentinel-2a-tile-7680x-10240y/timeseries/7680-10240-TCI-2019-08-09.png'
 # Outputs image file to current directory
 # s3.Bucket(BUCKET_NAME).download_file(KEY, 'current_satellite_image.jpg')
+
+# Read the binary datafile
+bandwidth_vi_data = pd.read_feather('C:\\Users\\Gladiator\\Documents\\Good_Crop_Bad_Crop\\dashboardv3\\data\\result-7680x-10240y')
+
+# Instantiating the Dashboard Application
+app = dash.Dash(__name__)
 
 title = 'The date this image was captured: xx/xx/xx'
 img = io.imread('data\\current_satellite_image.jpg')
@@ -38,11 +38,12 @@ map_fig.update_layout(autosize=True, margin=dict(l=0, r=0, b=0, t=0))
 # UI components
 vi_radio = dcc.RadioItems(
     options=[
-        {'label': 'Phenological Stage', 'value': 'NDVI'},
-        {'label': 'Maturity', 'value': 'GNDVI'},
-        {'label': 'Plantation', 'value': 'SAVI'}
+        {'label': 'Normalized Difference Vegetation Index', 'value': 'NDVI'},
+        {'label': 'Green Normalized Difference Vegetation Index', 'value': 'GNDVI'},
+        {'label': 'Ssoil Adjusted Vegetation Index', 'value': 'SAVI'}
     ],
-    labelStyle={'display': 'flex'},
+    labelStyle={'display': 'flex', 
+                'color': 'white'},
     value='NDVI',
     id= 'vi--radio'
     )  
@@ -55,6 +56,18 @@ field_selection = dcc.Dropdown(
         {'label': 'Field 3', 'value': 'Field_3'}
             ],
     value='Field_1'
+)
+
+time_scrub = dcc.Slider(
+    id='date_slider',
+    #min=bandwidth_vi_data['date'].min(),
+    min=2021,
+    #max=bandwidth_vi_data['date'].max(),
+    max=2026,
+    #value=bandwidth_vi_data['date'].min(),
+    value=2021,
+    marks={2021: '2021', 2022: '2022', 2023: '2023', 2024: '2024', 2025: '2025'},
+    step=1,
 )
 
 map = dcc.Graph(
@@ -71,13 +84,25 @@ chart = dcc.Graph(
     config={'displayModeBar': False}
     )
 
+# Function to get mask from basket (takes as inputs the Coordinates)
+def get_mask(x, y):
+    mask_path = f'satellite-data/phase-01/data/sentinel-2a-tile-{x}x-{y}y/masks/sugarcane-region-mask.png'
+    pic_bytes = s3.Object(BUCKET_NAME, mask_path).get()['Body'].read()
+    im = Image.open(io.BytesIO(pic_bytes))
+    arr = np.array(im.getdata()).reshape(512,512,4)
+    return arr
+
 # HTML Layout
 app.layout = html.Div(id='container', 
                     children = [
                                 html.Div(id = 'header', 
                                         children = [
                                                     html.H1('Good Crop, Bad Crop'),
-                                                    html.P('Predicting sugarcane health near Prosperine, Queensland')]
+                                                    ]
+                                        ),
+                                html.Div(id= 'header2',
+                                         children = [ 
+                                                    html.H2('Predicting sugarcane health near Prosperine, Queensland')]
                                         ),
                                 html.Div(id = 'sidebar', 
                                         children =[
@@ -88,13 +113,20 @@ app.layout = html.Div(id='container',
                                                             style={'font-weight': 'bold'}
                                                             ),
                                                     vi_radio,
+                                                    # Add blank line
+                                                    html.Br(), 
                                                     html.P('Select the region of interest:',
                                                             style={'font-weight': 'bold'}),
-                                                    field_selection
+                                                    field_selection,
+                                                    html.Br(), 
+                                                    html.P('Forecast into:',
+                                                            style={'font-weight': 'bold'}),
+                                                    time_scrub
                                                 ],
                                         ),
                                 html.Div(id = 'map', 
-                                        children = [map]
+                                        children = [map],
+                                        style= {'background-color': 'rgb(5, 4, 37)'}
                                         ),
                                 html.Div(id = 'chart', 
                                         children = [chart]
@@ -111,23 +143,22 @@ def update_time_series(VI):
     # Read and subset the dataframe
     upper = VI + '_LOWER'
     lower = VI + '_UPPER'
-    bandwidth_vi_data = pd.read_feather('C:\\Users\\Gladiator\\Documents\\Good_Crop_Bad_Crop\\dashboardv3\\data\\result-7680x-10240y')
-    bandwidth_vi_data = bandwidth_vi_data[['date', VI, lower, upper]]
+    data_vi = bandwidth_vi_data[['date', VI, lower, upper]]
     #print(bandwidth_vi_data.head(3))
 
     # Create time_series figures
     fig = go.Figure([
                         go.Scatter( 
-                                    name='VI_score',
-                                    x=bandwidth_vi_data['date'],
-                                    y=bandwidth_vi_data[VI],
+                                    name=f'Average {VI} - all pixels',
+                                    x=data_vi['date'],
+                                    y=data_vi[VI],
                                     line=dict(color='rgb(0,100,80)'),
                                     mode='lines'
                                   ),
                         go.Scatter(
                                     name='Upper Bound',
-                                    x=bandwidth_vi_data['date'],
-                                    y=bandwidth_vi_data[upper],
+                                    x=data_vi['date'],
+                                    y=data_vi[upper],
                                     mode='lines',
                                     marker=dict(color="#444"),
                                     line=dict(width=0),
@@ -135,8 +166,8 @@ def update_time_series(VI):
                                 ),
                         go.Scatter(
                                     name='Lower Bound',
-                                    x=bandwidth_vi_data['date'],
-                                    y=bandwidth_vi_data[lower],
+                                    x=data_vi['date'],
+                                    y=data_vi[lower],
                                     marker=dict(color="#444"),
                                     line=dict(width=0),
                                     mode='lines',
@@ -162,10 +193,8 @@ def update_time_series(VI):
 def update_field(field): 
     if field == 'Field_1':
         img = io.imread('data\\current_satellite_image.jpg')
-        #print(img)
     else:
         img = io.imread('data\\masked_region.png')
-        #print(img)
 
     title = 'The date this image was captured: xx/xx/xx'
     map_fig = px.imshow(img)
